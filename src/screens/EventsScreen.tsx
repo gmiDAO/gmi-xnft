91cc556c9fcb594b44bc988b24d6dc827fb0a181
Ruben Colomina <rcolomina@gmail.com>
2023-09-10 23:48:54 +0100

    add event view and buttons on screen

--- src/screens/EventsScreen.tsx
+++ src/screens/EventsScreen.tsx
@@ -1,31 +1,39 @@
-import { Text, FlatList } from "react-native";
+import { Text, FlatList, Button, View } from "react-native";
 import tw from "twrnc";
 
 import { Screen } from "../components/Screen";
 
+import { useEffect, useState } from "react";
+
+const eventListings = ["Proposed","Upcoming","Past"];
+
 export function EventsScreen() {
-  const features = [
-    "tailwind",
-    "recoil",
-    "native styling",
-    "fetching code from an API",
-    "using a FlatList to render data",
-    "Image for both remote & local images",
-    "custom fonts",
-    "sign a transaction / message",
-    "theme hook with light/dark support",
-  ];
+
+
+  const [activeButton, setActiveButton] = useState(0);
+
+
+  const handleButtonClick = (buttonIndex: number) => {
+    setActiveButton(buttonIndex);
+};
 
   return (
-    <Screen>
-      <Text style={tw`mb-4`}>
-        You'll find several examples of how to build xNFTs using react-native:
-      </Text>
-      <FlatList
-        data={features}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <Text>- {item}</Text>}
-      />
-    </Screen>
+      <Screen>
+         <View style={{ flexDirection: "row", alignItems: "center" }}>
+
+         {
+        
+            eventListings.map((tab, index) => (             
+              <Button title={tab} onPress={() => handleButtonClick(index)} />))
+      
+          
+          }
+
+
+
+               
+        </View>
+      </Screen>
+
   );
 }
