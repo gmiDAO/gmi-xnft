242386803c251361d58aef2550e6f3e997add648
rcolomina <rcolomina@gmail.com>
2023-10-02 16:44:44 +0100

    replace div by View react xNFT

--- App.tsx
+++ App.tsx
@@ -17,47 +17,47 @@ const Content: React.FC = () => {
   };
 
   return (
-    <div className="content">
-      <div className="nav">
+    <View className="content">
+      <View className="nav">
         <Logo className="logo-instance" />
-        <div className="menu">
-          <div className="navlinks">
-            <div className="navlink">
-              <div className="text-wrapper-14">Events</div>
+        <View className="menu">
+          <View className="navlinks">
+            <View className="navlink">
+              <View className="text-wrapper-14">Events</View>
               <img className="line-2" alt="Line" src="/img/line.svg" />
-            </div>
-            <div className="text-wrapper-14">Leaderboard</div>
-          </div>
-          <div className="icons">
+            </View>
+            <View className="text-wrapper-14">Leaderboard</View>
+          </View>
+          <View className="icons">
             <Property1Notification className="icon-17" />
             <img className="pfp-2" alt="Pfp" src="/img/pfp-4.png" />
-          </div>
-        </div>
+          </View>
+        </View>
         <Search className="search-instance" />
-      </div>
-      <div className="div-4">
-        <div onClick={handleButtonClick} className="buttons">
+      </View>
+      <View className="Viewn-4">
+        <Viewn onClick={handleButtonClick} className="buttons">
           <Button property1="primary" text="Propose event" />
-          <div className="tabs">
-            <div className="tabs-2">
+          <Viewn className="tabs">
+            <Viewn className="tabs-2">
               <Tab className="design-component-instance-node" property1="active" />
               <Tab className="design-component-instance-node" property1="inactive" />
               <Tab className="design-component-instance-node" property1="inactive" text="Fortnite" />
               <Tab className="design-component-instance-node" property1="inactive" text="Clash Royale" />
-            </div>
-            <div className="tabs-2">
+            </Viewn>
+            <Viewn className="tabs-2">
               <Tab className="design-component-instance-node" property1="active" text1="Proposed" />
               <Tab className="design-component-instance-node" property1="inactive" text="Upcoming" />
               <Tab className="design-component-instance-node" property1="inactive" text="Past" />
-            </div>
-          </div>
-        </div>
-        <div className="events">
+            </Viewn>
+          </Viewn>
+        </Viewn>
+        <Viewn className="events">
           <Event className="design-component-instance-node" device="desktop" type="proposed" />
           <Event className="design-component-instance-node" device="desktop" type="proposed" />
-        </div>
-      </div>
-    </div>
+        </Viewn>
+      </Viewn>
+    </Viewn>
   );
 };
 
